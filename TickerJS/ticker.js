6544a7a6fa2c6abaf7779afee74c17fda85bb6ce
ideafin <mz.adam@gmail.com>
2012-09-10 16:23:17 -0500

    Basic functionality - ticker will animate and rotate through list of items

--- TickerJS/ticker.js
+++ TickerJS/ticker.js
@@ -1,3 +1,25 @@
-$(document).ready(function(){ 
-	
+$(document).ready(function(){
+		ticker();
 });
+
+function ticker(){
+	setTimeout(tickerExecutor, 2000);
+	var first = $('.ticker-item:first').text();
+	$('.ticker').append('<div class="ticker-item">'+first+'</div>');
+	$('.ticker').css('top', '-1em');
+	$('.ticker-item:first').remove();
+}
+
+function tickerExecutor(){
+	$('.ticker').animate({
+    	top: '-=2.3em'
+  	}, 1000 );
+
+  	$('.ticker').animate({
+    	top: '+=.5em'
+  	}, 200 );
+
+  	$('.ticker').animate({
+    	top: '-=.2em'
+  	}, 100 , ticker);
+}
